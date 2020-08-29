2623b4698351dfaea5af5b5e1fba389dfd49b065
hamedbh <hamedbh@users.noreply.github.com>
2020-08-29 09:09:28 +0100

    use branching for the scaled predictions

--- _targets.R
+++ _targets.R
@@ -172,25 +172,12 @@ tar_pipeline(
     ),
     # pull out the best parameters for each model type, fit a model on the 
     # training data, and collect predictions made with those parameters
+    # first elastic net
     tar_target(
         gem_elnet_best_params, 
         gem_elnet_tune %>%
             select_by_pct_loss(desc(penalty),
                                metric = "roc_auc",
-                               limit = 1)
-    ), 
-    tar_target(
-        gem_mars_best_params, 
-        gem_mars_tune %>%
-            select_by_pct_loss(num_terms,
-                               metric = "roc_auc",
-                               limit = 1)
-    ), 
-    tar_target(
-        gem_xgb_best_params, 
-        gem_xgb_tune %>% 
-            select_by_pct_loss(tree_depth, sample_size, mtry, 
-                               metric = "roc_auc", 
                                limit = 1)
     ), 
     tar_target(
@@ -200,33 +187,25 @@ tar_pipeline(
             fit(data = training(gem_split))
     ), 
     tar_target(
-        gem_mars_fit, 
-        gem_mars_wfl %>%
-            finalize_workflow(gem_mars_best_params) %>%
-            fit(data = training(gem_split))
-    ), 
-    tar_target(
-        gem_xgb_fit, 
-        gem_xgb_wfl %>%
-            finalize_workflow(gem_xgb_best_params) %>%
-            fit(data = training(gem_split))
-    ), 
-    tar_target(
         gem_elnet_preds, 
         gem_elnet_tune %>% 
             collect_predictions() %>% 
             semi_join(gem_elnet_best_params, 
                       by = c("penalty", "mixture"))
     ), 
+    # then MARS
     tar_target(
-        gem_elnet_platt, 
-        gem_elnet_preds %>% 
-            calibrate_prob_model(.pred_ideal, cut, method = "platt")
+        gem_mars_best_params, 
+        gem_mars_tune %>%
+            select_by_pct_loss(num_terms,
+                               metric = "roc_auc",
+                               limit = 1)
     ), 
     tar_target(
-        gem_elnet_iso, 
-        gem_elnet_preds %>% 
-            calibrate_prob_model(.pred_ideal, cut, method = "isotonic")
+        gem_mars_fit, 
+        gem_mars_wfl %>%
+            finalize_workflow(gem_mars_best_params) %>%
+            fit(data = training(gem_split))
     ), 
     tar_target(
         gem_mars_preds, 
@@ -235,15 +214,19 @@ tar_pipeline(
             semi_join(gem_mars_best_params, 
                       by = c("num_terms"))
     ), 
+    # finally XGBoost
     tar_target(
-        gem_mars_platt, 
-        gem_mars_preds %>% 
-            calibrate_prob_model(.pred_ideal, cut, method = "platt")
+        gem_xgb_best_params, 
+        gem_xgb_tune %>% 
+            select_by_pct_loss(tree_depth, sample_size, mtry, 
+                               metric = "roc_auc", 
+                               limit = 1)
     ), 
     tar_target(
-        gem_mars_iso, 
-        gem_mars_preds %>% 
-            calibrate_prob_model(.pred_ideal, cut, method = "isotonic")
+        gem_xgb_fit, 
+        gem_xgb_wfl %>%
+            finalize_workflow(gem_xgb_best_params) %>%
+            fit(data = training(gem_split))
     ), 
     tar_target(
         gem_xgb_preds, 
@@ -252,14 +235,89 @@ tar_pipeline(
             semi_join(gem_xgb_best_params, 
                       by = c("mtry", "sample_size", "tree_depth"))
     ), 
+    # Now build calibrated versions of each of the models using their 
+    # predictions
+    # first put the models and predictions in lists for branching
     tar_target(
-        gem_xgb_platt, 
-        gem_xgb_preds %>% 
-            calibrate_prob_model(.pred_ideal, cut, method = "platt")
+        fit_list, 
+        list(
+            elnet = gem_elnet_fit, 
+            mars = gem_mars_fit, 
+            xgb = gem_xgb_fit
+        ), 
+        iteration = "list"
     ), 
     tar_target(
-        gem_xgb_iso, 
-        gem_xgb_preds %>% 
-            calibrate_prob_model(.pred_ideal, cut, method = "isotonic")
+        pred_list, 
+        list(
+            elnet = gem_elnet_preds, 
+            mars = gem_mars_preds, 
+            xgb = gem_xgb_preds
+        ), 
+        iteration = "list"
+    ), 
+    # then build the platt and isotonic versions
+    tar_target(
+        platt_mods,
+        calibrate_fit_platt(
+            pred_list, 
+            .pred_ideal, 
+            cut
+        ),
+        pattern = map(pred_list), 
+        iteration = "list"
+    ), 
+    tar_target(
+        iso_mods,
+        calibrate_fit_iso(
+            pred_list, 
+            .pred_ideal, 
+            cut
+        ),
+        pattern = map(pred_list), 
+        iteration = "list"
+    ), 
+    # Now generate predictions on the test data
+    tar_target(
+        unscaled_preds, 
+        predict(
+            fit_list, 
+            new_data = testing(gem_split), 
+            type = "prob"
+        ) %>% 
+            bind_cols(
+                testing(gem_split) %>% 
+                    select(cut)
+            ), 
+        pattern = map(fit_list), 
+        iteration = "list"
+    ), 
+    # Then scale with Platt and iso methods
+    tar_target(
+        platt_scaled_preds, 
+        predict(
+            platt_mods, 
+            unscaled_preds, 
+            type = "prob"
+        ) %>% 
+            bind_cols(
+                testing(gem_split) %>% 
+                    select(cut)
+            ), 
+        pattern = map(platt_mods, unscaled_preds), 
+        iteration = "list"
+    ), 
+    tar_target(
+        iso_scaled_preds, 
+        predict(
+            iso_mods, 
+            unscaled_preds
+        ) %>% 
+            bind_cols(
+                testing(gem_split) %>% 
+                    select(cut)
+            ), 
+        pattern = map(iso_mods, unscaled_preds), 
+        iteration = "list"
     )
 )
